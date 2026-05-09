710c6e5b4e229e5a061d12d9d06003a8915842ba
Ooozari <uzairasiff1227@gmail.com>
2026-05-09 13:57:53 +0500

    refactor: restructure index.js and enhance index.html with styling and messaging functionality

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const res = require('express/lib/response');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static(path.resolve('./public')));
+// Sockets.io
+io.on("connection", (socket) => {
+  // client send message to server
+  socket.on("user-message", ({ message }) => {
+    // server pass the message to all user except sender
+    socket.broadcast.emit("message", { message, isSender: false });
+  });
+});
 
-app.get('/', (req, res) => {
-    res.sendFile('/public/index.html');
-})
+// Express for http requests
+app.use(express.static(path.resolve("./public")));
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+});
 
-app.listen(9000, () => console.log("Server is running..."));
+server.listen(9000, () => console.log("Server is running..."));
